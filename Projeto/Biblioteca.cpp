3f2f4e30ac5e57ec577e47cc3d07805495ea45a9
Chris D.C <94999762+ChristianDziurza@users.noreply.github.com>
2024-12-10 19:37:58 -0300

    Update Biblioteca.cpp
    Ta feito a planta :)

--- Projeto/Biblioteca.cpp
+++ Projeto/Biblioteca.cpp
@@ -1,102 +1,68 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 typedef struct{
-    char nome[5];
+    char nome[30];
     double agua;
-    int vida = 10;
-    string desenvolvimento[3] = {"Semente", "Broto", "Adulto"};
-    string reino;
-    char indicativo;
-    int x, y;
+    int vida;
+    string desenvolvimento;
+    char reino[30];
+    int indicativo;
+    int x,y;
+    int idade;
 }Planta;
 
-void CriarMatriz( int n);
-Planta* bibloteca();
-void CriarPlanta(int n);
+Planta* biblioteca();
+void CriarPlanta(int n, int i);
+string nomePlanta();
+string definirReino();
 
 int main(){
-    Planta *planta = bibloteca();
-    int n, np;
-    cout << "Qual Tamanho" << endl;
+    srand(time(NULL));
+    Planta *planta = biblioteca();
+    int n;
     cin >> n;
-    cout << "Quantas plantas voce quer criar?" << endl;
-    cin >> np;
-    for(int i=0;i < np;i++){
-        CriarPlanta(n);
-        CriarMatriz(n);
+    for(int i=0; i<5;i++){
+        CriarPlanta(n,i);
+        cout << planta[i].nome << endl;
+        cout << planta[i].reino << endl;
+        cout << planta[i].desenvolvimento << endl;
+        cout << planta[i].agua << endl;
+        cout << planta[i].vida << endl;
+        cout << planta[i].idade << endl;
+        cout << planta[i].indicativo << endl;
+        cout << endl << endl;
     }
-    //TESTE
-    /*
-    string nome2;
-    for(int i=0;i<4;i++){
-        cin >> nome2;
-        for(int j=0;j<5;j++){
-            (planta+i)->nome[j] = nome2[j];
-        }
-    }
-    cout << "||||||" << endl;
-    for(int i=0;i<4;i++){
-        for(int j=0;j<5;j++){
-            cout << (planta+i)->nome[j];
-        }
-        cout << endl;
-    }
-    */
-    return 0;
+    
+     return 0;
 }
 
-Planta* bibloteca(){
-    static Planta inventario[4];
-    return &inventario[4];
+Planta* biblioteca(){
+    static Planta inventario[5];
+    return &inventario[5];
 }
-void CriarPlanta(int n){
-    string nome1;
-    Planta *planta = bibloteca();
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<n; j++){
-            planta[i].x = -20;
-            planta[i].y = -20;
-        }
-    }
-    int posicao=-1;
-    cout << "Qual posicao da planta" << endl;
-    while(posicao<0 || posicao > 4){
-        cin >> posicao;
-    }
-    cout << "Nome: " << endl;
-    cin >> nome1;
-    for(int i =0; i<5; i++){
-        planta[posicao].nome[i] = nome1[i];
-    }
-    cout << "Qual posicao no mapa" << endl;
-    int x=-1, y=-1; 
-    while(x<=0 || y<=0 || x > n || y>n){
-        cin >> x >> y;
-        x--;
-        y--;
-    }
-    planta[posicao].x = x;
-    planta[posicao].y = y;
+void CriarPlanta(int n, int i){
+    string nomeP = nomePlanta();
+    string nomeR = definirReino();
+    Planta *ponteiro_planta = biblioteca();
+    for(int j=0;j < sizeof(nomeP)/sizeof(nomeP[0]);j++)
+        ponteiro_planta[i].nome[j] = nomeP[j];
+    for(int j=0;j < sizeof(nomeR)/sizeof(nomeR[0]);j++)
+        ponteiro_planta[i].reino[j] = nomeR[j];
+    ponteiro_planta[i].desenvolvimento = "Semente";
+    ponteiro_planta[i].agua = 100;
+    ponteiro_planta[i].idade = 0;
+    ponteiro_planta[i].vida = 10;
+    ponteiro_planta[i].indicativo = i+1;
 }
-void CriarMatriz(int n){
-    char Matriz[n][n] = {'-'};
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<n; j++){
-            Matriz[i][j] = '-';
-        }
-    }
-    Planta *plantas = bibloteca();
-    
-    for(int i = 0; i<4; i++){
-        int x = plantas[i].x, y = plantas[i].y;
-        if(x != -20)
-            Matriz[x][y] = plantas[i].nome[0];
-    }
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<n; j++){
-            cout << Matriz[i][j] << " ";
-        }
-        cout << endl;
-    }
+
+string nomePlanta(){
+    string nomeP[4] = {"coisas","aleatorias","so","paratestar"};
+    return nomeP[rand()%4];
+}
+
+string definirReino(){
+    string nomeR[4] = {"fungus","mamidors","vegetal","suamae"};
+    return nomeR[rand()%4];
 }
